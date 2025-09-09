2be5a7278453c365967f816d47942f7d8d825e15
Lucivln <lakshminarasimha.151004@gmail.com>
2025-09-09 01:23:01 -0400

    feat(terraform): add cloudtrail logging to s3

--- terraform/main.tf
+++ terraform/main.tf
@@ -32,3 +32,18 @@ resource "aws_s3_bucket" "logs" {
   }
 }
 
+resource "aws_cloudtrail" "cloud_ir_trail" {
+  name                          = "cloud-ir-trail"
+  s3_bucket_name                = aws_s3_bucket.logs.bucket
+  include_global_service_events = true
+  is_multi_region_trail         = true
+  enable_logging                = true
+
+  event_selector {
+    read_write_type           = "All"
+    include_management_events = true
+  }
+
+  depends_on = [aws_s3_bucket.logs]
+}
+
